3db488d251255237db5dc3fa3f6813fff79e4919
M.D <rofayda321@gmail.com>
2020-04-09 17:41:55 -0200

    .

---
+++ app/src/main/java/com/example/theproject22/subject_activity.java
@@ -0,0 +1,14 @@
+package com.example.theproject22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class subject_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject_activity);
+    }
+}
