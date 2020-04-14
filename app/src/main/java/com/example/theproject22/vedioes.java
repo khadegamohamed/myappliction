b7de5246c14778ca2a7b75d0d1a0084a7d2a1b9c
Esraa555 <e658808@gmail.com>
2020-04-15 00:09:18 +0200

    git

---
+++ app/src/main/java/com/example/theproject22/vedioes.java
@@ -0,0 +1,14 @@
+package com.example.theproject22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class vedioes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vedioes);
+    }
+}
